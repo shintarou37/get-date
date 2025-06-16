022415b69a88ac770199c485878a238e6093fc0f
shintarou37 <m.sintarou37@gmail.com>
2025-06-16 22:47:56 +0100

    [feat] Add offsetMinutesSimple func

--- src/index.ts
+++ src/index.ts
@@ -9,3 +9,15 @@ export function offsetHoursSimple(offset: number): Date {
   const msPerHour = 60 * 60 * 1000;
   return new Date(Date.now() + offset * msPerHour);
 }
+
+/**
+ * offsetMinutesSimple
+ * @param offset  Number of minutes to offset from the current time.
+ *                  - Negative: move into the past
+ *                  - Positive: move into the future
+ * @returns       A new Date object representing the time offset by the given minutes.
+ */
+export function offsetMinutesSimple(offset: number): Date {
+  const msPerMin = 60 * 1000;
+  return new Date(Date.now() + offset * msPerMin);
+}
